a139f0b5f5f32e20105d5e367d0ea42d49478c10
andrewpetrovic <andrewpetrovic@gmail.com>
2015-08-02 16:12:53 +0800

    LogUtils完成注释

--- appbase/src/main/java/com/itic/mobile/util/logcat/LogUtils.java
+++ appbase/src/main/java/com/itic/mobile/util/logcat/LogUtils.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.itic.mobile.util.logcat;
 
 import android.util.Log;
@@ -21,8 +5,11 @@ import android.util.Log;
 import com.itic.mobile.BuildConfig;
 import com.itic.mobile.Config;
 
+/**
+ * LogUtils
+ */
 public class LogUtils {
-    private static final String LOG_PREFIX = "iosched_";
+    private static final String LOG_PREFIX = "itic_";
     private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
     private static final int MAX_LOG_TAG_LENGTH = 23;
 
