6eecdd693889aaff972d283bb66da494646a5e3a
andrewpetrovic <andrewpetrovic@gmail.com>
2015-08-11 11:39:32 +0800

    导入AppDataHandler,创建AppdataHandlerImpl,继承AppDataHandler,在mappingJsonHandler中将*Handler配置在其中

---
+++ demodata/src/main/java/com/example/andrea/demodata/AppDataHandlerImpl.java
@@ -0,0 +1,24 @@
+package com.example.andrea.demodata;
+
+import android.content.Context;
+import android.net.Uri;
+
+/**
+ * Created by andrea on 2015/8/10.
+ */
+public class AppDataHandlerImpl extends AppDataHandler {
+
+    private ContactHandler mContactHandler;
+    private static final String DATA_KEY_CONTACTS = "contacts";
+
+
+    public AppDataHandlerImpl(Context ctx) {
+        super(ctx, Contract.BASE_CONTENT_URI);
+        setTopLevelPath(Contract.TOP_LEVEL_PATHS);
+    }
+
+    @Override
+    public void mappingJsonHandler() {
+        mHandlerForKey.put(DATA_KEY_CONTACTS,mContactHandler = new ContactHandler(mContext));
+    }
+}
